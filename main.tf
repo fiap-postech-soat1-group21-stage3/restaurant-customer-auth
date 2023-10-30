abb197a406c0cb55e147ddb6ab72db41152b6305
b-bianca <bianca.cms@outlook.com.br>
2023-10-30 19:53:20 -0300

    test-tf

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
- 
  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
  path         = "/"
  description  = "AWS IAM Policy for managing aws lambda role"
